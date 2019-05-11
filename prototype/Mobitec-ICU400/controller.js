64c518540b089b76106b8ac39bfe1be6b447aa18
Eyeball Edward <edwardyeung.private@gmail.com>
2019-05-11 21:12:51 +1000

    icu400

--- prototype/Mobitec-ICU400/controller.js
+++ prototype/Mobitec-ICU400/controller.js
@@ -1,6 +1,7 @@
 let EDSFormats = {};
 let EDSData = {};
 let EDSImages = {};
+let EDSExtras = {};
 
 let screenWidth = 146,
     screenHeight = 40;
@@ -20,9 +21,16 @@ let currentCanvasData = null;
 
 let canvasScale = 51; // DAMMIT SAFARI YOURE A NUTCASE
 
-let code = [0, 0, 0, 0];
+let code = [0, 0, 0, 1];
+let extra = [0, 0];
+
+let inputMode = 'Dest';
+
+let idleTimeout = 0;
 
 let keyTimeout = 0;
+
+let operator = 'SMRT';
 
 function setDisplayText(lines) {
     screenMatrix.onBeginDraw();
@@ -41,60 +49,104 @@ function drawFrame(text) {
     screenMatrix.onEndDraw();
 }
 
-function parse(code) {
-    if (!EDSData.SMRT[code]) return null;
-    return parseFormat(EDSFormats.SMRT, EDSData.SMRT[code].front, EDSImages.SMRT, innerDisplay);
+function parse(code, extraCode, extraFirst) {
+    if (!EDSData[operator][code]) return null;
+    if (!EDSExtras[operator][extraCode] && extraFirst) return null;
+
+    let extraData;
+
+    let data = parseFormat(EDSFormats[operator], EDSData[operator][code].front, EDSImages[operator], innerDisplay);
+    if (!!EDSExtras[operator][extraCode])
+        extraData = parseFormat(EDSFormats[operator], EDSExtras[operator][extraCode].front, EDSImages[operator], innerDisplay);
+
+    if (extraData) {
+        if (extraFirst)
+            data.pages = extraData.pages.concat(data.pages);
+        else
+            data.pages = data.pages.concat(extraData.pages);
+
+        data.scrollSpeed = data.scrollSpeed === -1 ? 3000 : data.scrollSpeed;
+    }
+
+    return data;
 }
 
 function keyPress(num) {
     let d = num / Math.abs(num);
-    let index = code.length - Math.abs(num);
+    let currentCode = inputMode === 'Dest' ? code : extra;
 
-    if (d === 1 && code[index] + 1 === 10) code[index] = -1;
-    else if (d === -1 && code[index] - 1 === -1) code[index] = 10;
+    let index = currentCode.length - Math.abs(num);
 
-    code[index] += d;
+    if (d === 1 && currentCode[index] + 1 === 10) currentCode[index] = -1;
+    else if (d === -1 && currentCode[index] - 1 === -1) currentCode[index] = 10;
 
-    setCodePreview(code);
+    currentCode[index] += d;
 
-    clearTimeout(keyTimeout);
-    keyTimeout = setTimeout(setCode.bind(null, code), 1500);
+    setPreviewCode(code, extra, inputMode === 'Extra');
 }
 
-function setCodePreview(code) {
-    drawFrame('Dest: ' + code.join(''));
-
-    if (currentCanvasData)
-        screenCanvasContext.putImageData(currentCanvasData, 1 * canvasScale, 19 * canvasScale);
-}
-
-function setCode(code) {
-    drawFrame('Dest: ' + code.join(''));
-
-    let parsed = parse(code.join('')*1+'');
-
+function setPreviewCode(code, extra, extraFirst) {
+    drawFrame(inputMode + ': ' + (inputMode === 'Dest' ? code : extra).join(''));
+    let parsed = parse(code.join('')*1+'', extra.join('')*1+'', extraFirst);
     render(parsed, innerDisplay);
-    if (!parsed) currentCanvasData = null;
+}
+
+function setupKey(element, short, long) {
+    let pressStart;
+
+    element.addEventListener('mousedown', e => {
+        e.preventDefault();
+        pressStart = performance.now();
+    });
+    element.addEventListener('mouseup', e => {
+        e.preventDefault();
+        let pressTime = performance.now() - pressStart;
+
+        if (pressTime < 500) {
+            short();
+        } else {
+            if (long) long();
+            else short();
+        }
+    });
+}
+
+function toggleDisplayInput() {
+    if (inputMode == 'Dest') inputMode = 'Extra';
+    else inputMode = 'Dest';
+
+    setPreviewCode(code, extra, inputMode === 'Extra');
 }
 
 function setup() {
     for (let p = 0; p <= 3; p++) {
         let place = Math.pow(10, p);
+        let pressStart;
 
-        document.getElementById(place + '-up').addEventListener('click', keyPress.bind(null, p + 1));
-        if (place !== 1000)
-            document.getElementById(place + '-down').addEventListener('click', keyPress.bind(null, -p - 1));
+        let shortPressUp = keyPress.bind(null, p + 1);
+        let shortPressDown = keyPress.bind(null, -p - 1);
+        let longHoldUp = undefined;
+        let longHoldDown = undefined;
+
+        if (place === 1000) {
+            longHoldDown = shortPressDown;
+            shortPressDown = toggleDisplayInput;
+
+            longHoldUp = () => {};
+        }
+
+        setupKey(document.getElementById(place + '-up'), shortPressUp, longHoldUp);
+        setupKey(document.getElementById(place + '-down'), shortPressDown, longHoldDown);
     }
 
-    code = [0, 1, 1, 7];
-    setCode(code);
+    setPreviewCode(code, extra);
 }
 
 function startup() {
     var textSets = [
-        ['Booting Sys', '', '', 'Boot: 1371-R7', 300],
-        ['Staring Application', '', '', 'Bios: 1372-R13', 300],
-        ['mobitec ICU 400', 'Version: 1373-R58', 300],
+        ['Booting Sys', '', '', 'Boot: 1371-R7', 500],
+        ['Staring Application', '', '', 'Bios: 1372-R13', 500],
+        ['mobitec ICU 400', 'Version: 1373-R58', 500],
         ['mobitec ICU 400', 'Version: 1373-R58', 'Prg: Testpgm', 500]
     ];
 
@@ -132,4 +184,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     hookDisplay(innerDisplay);
     startup();
+
+    setPreviewCode(code, extra);
 })
