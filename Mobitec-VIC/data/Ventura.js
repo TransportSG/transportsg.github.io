fc6879140048114f25806b36342cde2a9d7e0ae8
Eyeball Edward <edwardyeung.private@gmail.com>
2019-06-15 07:05:20 +1000

    6038 flinders

--- Mobitec-VIC/data/Ventura.js
+++ Mobitec-VIC/data/Ventura.js
@@ -766,6 +766,36 @@ EDSData.Ventura = {
             scrollFont: 'Mobitec-6:5'
         }
     },
+    6038: {
+        front: {
+            renderType: "standardService",
+            serviceNumber: "",
+            destination: {
+                text: "Flinders Street",
+                font: "Mobitec-7:7"
+            },
+            scrolls: [
+                "Train Replacement",
+                "All Stations"
+            ],
+            scrollFont: 'Mobitec-6:5'
+        }
+    },
+    6138: {
+        front: {
+            renderType: "standardService",
+            serviceNumber: "",
+            destination: {
+                text: "Flinders Street",
+                font: "Mobitec-7:7"
+            },
+            scrolls: [
+                "Train Replacement",
+                "Limited Express"
+            ],
+            scrollFont: 'Mobitec-6:5'
+        }
+    },
     6039: {
         front: {
             renderType: "standardService",
