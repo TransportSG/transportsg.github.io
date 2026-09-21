e16eda662902fa4f98d933cb33382903d13b2533
Eyeball Edward <edwardyeung39@gmail.com>
2026-09-21 19:05:55 +1000

    split fonts up into separate files

---
+++ eds-fonts/fonts-template.js
@@ -0,0 +1,9 @@
+(function (exports) {
+  exports.fonts = {
+    ...(exports.fonts || {}),
+  }
+
+  exports.fontSpacers = {
+    ...(exports.fontSpacers || {}),
+  }
+})(typeof exports === 'undefined' ? window : exports);
